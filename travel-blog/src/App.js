ceab7b93f66dec492ce0ec0ce6898d8b280f7b7b
benjewman <bennyadave@gmail.com>
2020-11-23 14:00:34 -0500

    Add footer

--- travel-blog/src/App.js
+++ travel-blog/src/App.js
@@ -11,7 +11,7 @@ function App() {
           <h2 className="menu__title--margin">My traveling experiences</h2>
         </div>
         <div className="menu__navigation">
-          <button>About</button>
+          <button className="menu__navigation--focus">About</button>
           <button>Blog</button>
           <button>Home</button>
         </div>
@@ -26,6 +26,10 @@ function App() {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum posuere urna nec tincidunt praesent. Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Donec enim diam vulputate ut pharetra sit amet aliquam id. Tincidunt arcu non sodales neque sodales ut etiam sit. Donec ac odio tempor orci dapibus ultrices in. Volutpat ac tincidunt vitae semper quis lectus nulla at. Vitae sapien pellentesque habitant morbi. Mi eget mauris pharetra et ultrices neque. Orci a scelerisque purus semper eget duis at tellus. Nunc congue nisi vitae suscipit tellus mauris a diam maecenas. Eget nulla facilisi etiam dignissim. Et magnis dis parturient montes.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum. At augue eget arcu dictum. Diam maecenas ultricies mi eget mauris pharetra et ultrices neque. Egestas maecenas pharetra convallis posuere morbi leo. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Semper feugiat nibh sed pulvinar. Eu nisl nunc mi ipsum faucibus vitae. Ut diam quam nulla porttitor massa id neque aliquam vestibulum. Rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui. Lacinia at quis risus sed vulputate odio ut enim blandit. Aliquet enim tortor at auctor urna nunc. Posuere lorem ipsum dolor sit amet. Ultrices gravida dictum fusce ut placerat. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper.</p>
         </div>
+      </div>
+      <div className="footer">
+        <span className="footer__item">© 2020 Travelize</span>
+        <span className="footer__item">Privacy Policy Terms and conditions</span>
       </div>
     </div>
   );
